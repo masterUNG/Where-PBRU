5d17bbcfeec3e4167995f3e30659e959b9748869
masterUNG <phrombutr@gmail.com>
2016-05-27 15:23:35 +0700

    Center Marker

--- app/src/main/java/appewtc/masterung/wherepbru/MapsActivity.java
+++ app/src/main/java/appewtc/masterung/wherepbru/MapsActivity.java
@@ -7,12 +7,17 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private String[] resultStrings;
+    private int[] iconInts = {R.drawable.build1, R.drawable.build2,
+            R.drawable.build3, R.drawable.build4, R.drawable.build5,
+            R.drawable.build6};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +48,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         LatLng latLng = new LatLng(douLat, douLng);
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+
+        mMap.addMarker(new MarkerOptions()
+        .position(latLng)
+        .icon(BitmapDescriptorFactory
+        .fromResource(iconInts[Integer.parseInt(resultStrings[5])])));
 
     }   // setupCenter
 
